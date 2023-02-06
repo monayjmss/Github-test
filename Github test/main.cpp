facb12cb393a2115e836f4d966a8e674e54f3e6b
monayjmss <jamesm12@email.winthrop.edu>
2023-02-06 13:24:55 -0500

    Initial Commit

---
+++ Github test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Github test
+//
+//  Created by Monay James on 2/6/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
